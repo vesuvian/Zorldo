f36f5e74f95cb05eda7d3225c5ef2d78cf77da96
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-13 22:09:29 -0400

    Begin building protagonist game object

---
+++ Assets/Scripts/AbstractMonoBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	/// <summary>
+	/// Simple abstraction to expose Unity3D event hooks.
+	/// </summary>
+	public abstract class AbstractMonoBehaviour : MonoBehaviour
+	{
+		/// <summary>
+		/// Called once per frame.
+		/// </summary>
+		protected virtual void Update()
+		{
+		}
+	}
+}
---
+++ Assets/Scripts/Protagonist.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	/// <summary>
+	/// The manliest of men.
+	/// </summary>
+	public sealed class Protagonist : AbstractMonoBehaviour
+	{
+		[SerializeField]
+		private int m_Speed;
+
+		/// <summary>
+		/// Called once per frame.
+		/// </summary>
+		protected override void Update()
+		{
+			Vector2 inputVector = Vector2.zero;
+
+			if (Input.GetKey(KeyCode.W))
+				inputVector += Vector2.up;
+			if (Input.GetKey(KeyCode.S))
+				inputVector += Vector2.down;
+			if (Input.GetKey(KeyCode.A))
+				inputVector += Vector2.left;
+			if (Input.GetKey(KeyCode.D))
+				inputVector += Vector2.right;
+
+			inputVector = inputVector.normalized;
+
+			Vector2 moveVector = inputVector * m_Speed * Time.deltaTime;
+
+			transform.Translate(moveVector);
+		}
+	}
+}
