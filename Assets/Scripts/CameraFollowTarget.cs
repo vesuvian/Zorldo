3ad6d95198190eabc2f56046860c3a7a89cc4a7f
Jeffery Thompson <jlthompson3259@gmail.com>
2018-04-15 15:53:54 -0400

    Camera follows the player

---
+++ Assets/Scripts/CameraFollowTarget.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class CameraFollowTarget : AbstractMonoBehaviour
+	{
+
+		/// <summary>
+		/// GameObject that the camera should follow
+		/// </summary>
+		[SerializeField]
+		private GameObject _target;
+
+		// Update is called once per frame
+		protected override void Update()
+		{
+			var cameraTransform = GetComponent<Transform>();
+			Vector2 difference = _target.GetComponent<Transform>().position - cameraTransform.position;
+
+			cameraTransform.Translate(difference);
+		}
+	}
+}
