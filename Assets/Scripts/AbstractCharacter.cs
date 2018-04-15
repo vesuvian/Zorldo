fc9e64701d6d4b34bc6530ebe245a39df539991a
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-15 17:11:47 -0400

    Initial commit of AbstractCharacter

---
+++ Assets/Scripts/AbstractCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public abstract class AbstractCharacter : AbstractMonoBehaviour
+	{
+		[SerializeField] private int m_Speed;
+
+		/// <summary>
+		/// Called once per frame.
+		/// </summary>
+		protected override void Update()
+		{
+			GetComponent<Rigidbody2D>().velocity = GetDirectionVector() * m_Speed * Time.deltaTime;
+		}
+
+		/// <summary>
+		/// Gets the direction to move this frame.
+		/// </summary>
+		/// <returns></returns>
+		protected abstract Vector2 GetDirectionVector();
+	}
+}
