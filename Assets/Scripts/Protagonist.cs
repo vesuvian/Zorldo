8ede7799b01dc6736e90fcc454f6deb48effaffe
Jeffery Thompson <jlthompson3259@gmail.com>
2018-04-15 16:15:06 -0400

    Add tilemap collider, fix null ref

--- Assets/Scripts/Protagonist.cs
+++ Assets/Scripts/Protagonist.cs
@@ -38,9 +38,12 @@ namespace Assets.Scripts
 
 		private void OnCollisionEnter2D(Collision2D collision)
 		{
-			Vector2 collisionDirection = collision.otherRigidbody.position - collision.rigidbody.position;
-			collisionDirection = collisionDirection.normalized;
-			GetComponent<Rigidbody2D>().velocity = collisionDirection * m_HitVelocity;
+			if (collision.otherRigidbody != null && collision.rigidbody != null)
+			{
+				Vector2 collisionDirection = collision.otherRigidbody.position - collision.rigidbody.position;
+				collisionDirection = collisionDirection.normalized;
+				GetComponent<Rigidbody2D>().velocity = collisionDirection * m_HitVelocity;
+			}
 		}
 	}
 }
